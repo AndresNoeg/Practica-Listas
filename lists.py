f5cae2c717798901b5718a272863a01d2422c024
Gomez Garcia Andres Noe <a.23308051281184@cbtis128.edu.mx>
2024-09-24 20:20:26 -0600

    Practica Listas

---
+++ lists.py
@@ -0,0 +1,23 @@
+print(" ")
+print("Gomez Garcia Andres Noe: Practica listas")
+print(" ")
+#Lista de datos constantes o caracteresen una variable
+thislist = ["Apple", "Banana", "Cherry"]
+#Imprime la lista declarada
+print(thislist)
+print(" ")
+#Al agregar len nos dice la cantidad de articulos que hay en esa lista
+print(len(thislist))
+print(" ")
+
+#Declara otras 2 listas
+list2 = [1,5,7,9,3]
+list3 = [True, False, True]
+print(" ")
+
+#Imprime las listas declaradas
+print(thislist)
+print(" ")
+print(list2)
+print(" ")
+print(list3)
